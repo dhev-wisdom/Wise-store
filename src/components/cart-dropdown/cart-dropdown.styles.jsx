10e359ec877d09af0caba0da6aaf437afe01dc46
Chinonso Wisdom <nonsowisdom62@gmail.com>
2025-06-30 00:45:15 +0100

    margins and paddings

--- src/components/cart-dropdown/cart-dropdown.styles.jsx
+++ src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -9,15 +9,36 @@ export const CartDropdownContainer = styled.div`
     border: 1px solid black;
     background-color: white;
     position: absolute;
-    top: 90px;
+    top: 102px;
     right: 40px;
     z-index: 5;
+
+    @media (max-width: 639px) {
+        top: 91px;
+        right: 13px;
+    }
+
+    @media (min-width: 640px) and (max-width: 767px) {
+        top: 92px;
+        right: 15px;
+    }
+
+    @media (min-width: 768px) and (max-width: 1023px) {
+        top: 96px;
+        right: 25px;
+    }
+
+    @media (min-width: 1536px) {
+        top: 107px;
+        right: 50px;
+    }
 
     ${BaseButton}, ${InvertedButton}, ${GoogleSigninButton} {
         margin-top: auto;
         text-transform: capitalize;
         font-size: 12px;
     }
+
 `;
 
 export const EmptyMessage = styled.span`
