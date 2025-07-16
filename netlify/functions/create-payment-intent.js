4367d863bbb8ff26e1d6c0af18435595c5092b3b
Chinonso Wisdom <nonsowisdom62@gmail.com>
2025-07-16 23:23:30 +0100

    push without s...

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,29 +1,3 @@
-// require("dotenv").config();
-// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
-
-// exports.handler = async(event) => {
-//     try{
-//         const { amount } = JSON.parse(event.body);
-//         const paymentIntent = await stripe.paymentIntent.create({
-//             amount,
-//             currency: "usd",
-//             payment_method_types: ["card"],
-//         })
-
-//         return {
-//             statusCode: 200,
-//             body: JSON.stringify({ paymentIntent })
-//         }
-//     } catch (error) {
-//         console.error({ error });
-//         return {
-//             statusCode: 400,
-//             body: JSON.stringify({ error }),
-//         }
-//     }
-// }
-
-// Use import/export syntax
 import 'dotenv/config'; // Modern way to load dotenv in ESM
 import Stripe from 'stripe'; // Import Stripe as a default export
 
