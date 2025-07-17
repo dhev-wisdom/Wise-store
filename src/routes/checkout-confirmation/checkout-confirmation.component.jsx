f640f0d5ec4d11aac1148b893d839e29db336ff6
Chinonso Wisdom <nonsowisdom62@gmail.com>
2025-07-17 10:06:36 +0100

    optimize

--- src/routes/checkout-confirmation/checkout-confirmation.component.jsx
+++ src/routes/checkout-confirmation/checkout-confirmation.component.jsx
@@ -1,7 +1,6 @@
 import Button, { BUTTON_TYPE_CLASSES } from '../../components/button/button.component';
 import { Link } from 'react-router-dom';
-import './checkout-confirmation.styles.scss'
-import { BUTTON_TYPE_CLASSES } from '../../components/button/button.component';
+import './checkout-confirmation.styles.scss';
 
 const CheckoutConfirm = () => {
     return (
@@ -9,8 +8,8 @@ const CheckoutConfirm = () => {
         <p className="main-header">Thank you for shopping with <span>Wise Cloth Store</span></p>
         <p className="smaller-header">We hope to see you soon</p>
         <div className='button-container'>
-            <Button><Link to='/'>Go Home</Link></Button>
-            <Button buttonType={BUTTON_TYPE_CLASSES.inverted}><Link to='/shop'>Shop More</Link></Button>
+            <Button><Link to='/'>GO HOME</Link></Button>
+            <Button buttonType={BUTTON_TYPE_CLASSES.inverted}><Link to='/shop'>SHOP MORE</Link></Button>
         </div>
         </div>
     )
